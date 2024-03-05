813f6e6441d98b6eb97c9c2bad0c7e7f5656fe57
Jeremiah Donley <108106416+WeiQiPro@users.noreply.github.com>
2024-03-05 13:23:33 -0500

    Update canvas.js
    better readability and updated controls

--- src/canvas.js
+++ src/canvas.js
@@ -2,88 +2,105 @@ class Canvas {
   constructor(element) {
     this.canvas = document.getElementById(element);
     this.context = this.canvas.getContext('2d');
+    this.initializeCanvas();
+    this.stones = [];
+    this.bindEventListeners();
+  }
+
+  initializeCanvas() {
     this.canvas.width = CONST.CANVAS.WIDTH;
     this.canvas.height = CONST.CANVAS.HEIGHT;
-    this.stones_radius = document.getElementById('stonesize').value
-    this.stones = [];
+  }
 
-    // bind event listeners
-    this.canvas.addEventListener('mousedown', this.handle_mouse_down.bind(this));
-    this.canvas.addEventListener('contextmenu', this.handle_context_menu.bind(this));
-    this.canvas.addEventListener('mousedown', this.handle_clearing_stones.bind(this));
+  bindEventListeners() {
+    // Binding mouse and custom event listeners
+    this.canvas.addEventListener('mousedown', this.handleMouseDown.bind(this));
+    this.canvas.addEventListener('contextmenu', this.handleContextMenu.bind(this));
+    document.addEventListener('keydown', this.handleKeyDown.bind(this));
   }
 
   tick() {
-    this.stones_radius = document.getElementById('stonesize').value
-    let last_item_index = this.stones.length - 1;
-    this.clear_canvas();
-    for (const stone of this.stones) {
-      this.draw_circle(stone);
-    }
-    if (this.stones[last_item_index] !== undefined) {
-      let stone_to_be_marked = this.stones[last_item_index];
-      this.draw_marker(stone_to_be_marked);
-    }
+    this.updateStonesRadius();
+    this.clearCanvas();
+    this.drawStones();
+    this.markLastStone();
   }
 
+  updateStonesRadius() {
+    this.stones_radius = document.getElementById('stonesize').value;
+  }
 
-  clear_canvas() {
+  clearCanvas() {
     this.context.clearRect(0, 0, CONST.CANVAS.WIDTH, CONST.CANVAS.HEIGHT);
   }
 
-  draw_circle([mouse_x, mouse_y, stone_color]) {
-    let offset = this.stones_radius / 2
-    this.context.drawImage(stone_color, mouse_x - offset, mouse_y - offset, this.stones_radius, this.stones_radius)
+  drawStones() {
+    this.stones.forEach(stone => this.drawCircle(stone));
   }
 
-  draw_marker([mouse_x, mouse_y, stone_color]){
-    let marker_size = this.stones_radius / 2
-    let offset = this.stones_radius / 4
-    this.context.drawImage(STONES.MARKER, mouse_x - offset, mouse_y - offset, marker_size, marker_size)
+  markLastStone() {
+    if (this.stones.length > 0) {
+      const lastStone = this.stones[this.stones.length - 1];
+      this.drawMarker(lastStone);
+    }
   }
 
-  handle_mouse_down(event) {
+  drawCircle([mouse_x, mouse_y, stone_color]) {
+    let offset = this.stones_radius / 2;
+    this.context.drawImage(stone_color, mouse_x - offset, mouse_y - offset, this.stones_radius, this.stones_radius);
+  }
+
+  drawMarker([mouse_x, mouse_y, stone_color]) {
+    let marker_size = this.stones_radius / 2;
+    let offset = this.stones_radius / 4;
+    this.context.drawImage(STONES.MARKER, mouse_x - offset, mouse_y - offset, marker_size, marker_size);
+  }
+
+  handleMouseDown(event) {
     let { left, top } = this.canvas.getBoundingClientRect();
     let [x, y] = this.getCanvasCoords(event.clientX - left, event.clientY - top);
 
-    if (event.button === 0) { // left click
+    if (event.button === 0) { // Left click
       this.stones.push([x, y, STONES.BLACK]);
-    } else if (event.button === 2) { // right click
+    } else if (event.button === 2) { // Right click
       this.stones.push([x, y, STONES.WHITE]);
     }
 
-    this.check_for_overlapping_stones()
+    this.checkForOverlappingStones();
   }
 
-  handle_context_menu(event) {
+  handleContextMenu(event) {
     event.preventDefault();
   }
 
-  handle_clearing_stones(event) {
-    event.preventDefault()
-    if (event.button === 1) { // middle click
-      this.stones = [];
+  handleKeyDown(event) {
+    if (event.code === 'Space') {
+      this.clearStones();
+      event.preventDefault();
     }
   }
 
-  check_for_overlapping_stones() {
-    for (let i = 0; i < this.stones.length; i++) {
+  clearStones() {
+    this.stones = [];
+    this.clearCanvas();
+  }
+
+  checkForOverlappingStones() {
+    // Checking for overlapping stones and removing overlaps
+    this.stones = this.stones.filter((stone, i) => {
       for (let j = i + 1; j < this.stones.length; j++) {
-        const [stone_position_x1, stone_position_y1, stone_color_1] = this.stones[i];
-        const [stone_position_x2, stone_position_y2, stone_color_2] = this.stones[j];
-        const distance_between_stones_x = stone_position_x2 - stone_position_x1;
-        const distance_between_stones_y = stone_position_y2 - stone_position_y1;
-        const total_distance = Math.sqrt(distance_between_stones_x * distance_between_stones_x + distance_between_stones_y * distance_between_stones_y);
-        if (total_distance < 45) {
-          this.stones.splice(i, 1);
-          i--;
-          break;
+        if (this.isOverlapping(stone, this.stones[j])) {
+          return false;
         }
       }
-    }
+      return true;
+    });
   }
 
-
+  isOverlapping([x1, y1], [x2, y2]) {
+    const distance = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
+    return distance < this.stones_radius; // Assuming radius as the criterion for overlap
+  }
 
   getCanvasCoords(clientX, clientY) {
     let { width, height } = this.canvas.getBoundingClientRect();
