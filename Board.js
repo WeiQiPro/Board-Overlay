9f33300feeb9cf56a8da14ba1ede2c9d5c38bee9
seminyoon1 <seminyoon1@gmail.com>
2024-03-04 19:32:59 -0500

    Update Board.js
    Added a function to get the closest point

--- Board.js
+++ Board.js
@@ -5,8 +5,8 @@ function generateGrid(points) {
   // Generate an empty board 
   const grid = Array.from({ length: 19 }, () => Array.from({ length: 19 }).fill(0));
 
-  width_diff = (points[1][1] - points[0][1]) / 18
-  length_diff = (points[2][0] - points[0][0]) / 18
+  const width_diff = (points[1][1] - points[0][1]) / 18
+  const length_diff = (points[2][0] - points[0][0]) / 18
 
   //Fill the rest of the board using the differences
   for (let i = 0; i < 19; i++) {
@@ -19,3 +19,27 @@ function generateGrid(points) {
 
   return grid
 }
+
+function distance(x1, y1, x2, y2) {
+    return Math.sqrt((x1 - x2) ** 2 + (y1 - y2) ** 2);
+}
+
+// Function to find the closest point on the grid given an x and y coordinate, returns that coordinate.
+function findClosestPoint(x, y, grid) {
+    let closestPoint = grid[0][0];
+    let minDistance = distance(x, y, closestPoint[0], closestPoint[1]);
+
+    // Iterate through each point in the grid
+    grid.forEach(row => {
+        row.forEach(point => {
+            const dist = distance(x, y, point[0], point[1]);
+            // Update closest point if distance is smaller
+            if (dist < minDistance) {
+                minDistance = dist;
+                closestPoint = point;
+            }
+        });
+    });
+    return closestPoint;
+}
+
